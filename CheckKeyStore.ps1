f076723c69ef65ccdc5a5eea225a1bcc4a839063
MartinGutz <MartinGutz@gmail.com>
2020-10-21 19:24:43 -0500

    Add initial files to check Certs/KeyStore

---
+++ CheckKeyStore.ps1
@@ -0,0 +1,17 @@
+$KeyToolCommand = "C:\Program Files\Java\jre1.8.0_261\bin\keytool.exe"
+$TrustStoreLocation = "C:\Program Files\Java\jre1.8.0_261\lib\security\cacerts"
+$Password = "changeit"
+$results = $(& $KeyToolCommand -list -keystore $TrustStoreLocation -storepass "$Password" -v)
+Write-Host $results
+
+foreach($result in $results)
+{
+    if($result.Contains("Alias name:"))
+    {
+        Write-Host $result
+    }
+    if($result.Contains("Valid from:"))
+    {
+        Write-Host $result
+    }
+}
