97cff0e1342187e6b505f4924cc65ce305e5cf9d
Martin Gutierrez <martingutz@gmail.com>
2020-10-21 19:44:57 -0500

    Placed code into function

--- CheckKeyStore.ps1
+++ CheckKeyStore.ps1
@@ -1,19 +1,22 @@
-
-
-$KeyToolCommand = "C:\Program Files\Java\jre1.8.0_261\bin\keytool.exe"
-$TrustStoreLocation = "C:\Program Files\Java\jre1.8.0_261\lib\security\cacerts"
-$Password = "changeit"
-$results = $(& $KeyToolCommand -list -keystore $TrustStoreLocation -storepass "$Password" -v)
-Write-Host $results
-
-foreach($result in $results)
+function GetTrustStoreInformation()
 {
-    if($result.Contains("Alias name:"))
+    $KeyToolCommand = "C:\Program Files\Java\jre1.8.0_261\bin\keytool.exe"
+    $TrustStoreLocation = "C:\Program Files\Java\jre1.8.0_261\lib\security\cacerts"
+    $Password = "changeit"
+    $results = $(& $KeyToolCommand -list -keystore $TrustStoreLocation -storepass "$Password" -v)
+    Write-Host $results
+    foreach($result in $results)
     {
-        Write-Host $result
-    }
-    if($result.Contains("Valid from:"))
-    {
-        Write-Host $result
-    }
+        if($result.Contains("Alias name:"))
+        {
+            Write-Host $result
+        }
+        if($result.Contains("Valid from:"))
+        {
+            Write-Host $result
+        }
+    }   
 }
+
+GetTrustStoreInformation
+
