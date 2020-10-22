041be4e5be90f7b0672969b4586ba5136a882121
Martin Gutierrez <martingutz@gmail.com>
2020-10-21 20:51:24 -0500

    Added functions to parse date

--- Scripts/CheckKeyStore.ps1
+++ Scripts/CheckKeyStore.ps1
@@ -1,3 +1,31 @@
+function GetMonthNumber($monthInitials)
+{
+    SWITCH($monthInitials) {
+    "Jan" { return "01"}
+    "Feb" { return "02"}
+    "Mar" { return "03"}
+    "Apr" { return "04"}
+    "May" { return "05"}
+    "Jun" { return "06"}
+    "Jul" { return "07"}
+    "Aug" { return "08"}
+    "Sep" { return "09"}
+    "Oct" { return "10"}
+    "Nov" { return "11"}
+    "Dec" { return "12"}
+    }
+}
+
+function ParseExpirationDate($string)
+{
+    $dateSubstring = $string.Substring(48)
+    Write-Host $dateSubstring
+    $expirationDate = ( GetMonthNumber $dateSubstring.SubString(4,3)) + "/" + $dateSubstring.SubString(8,2) + "/" + $dateSubstring.SubString(24,4)
+    Write-Host $expirationDate
+    [datetime]$parsedDate = $expirationDate
+    Write-Host $parsedDate
+    return $parsedDate
+}
 function GetTrustStoreInformation()
 {
     $KeyToolCommand = "C:\Program Files\Java\jre1.8.0_261\bin\keytool.exe"
@@ -17,7 +45,7 @@ function GetTrustStoreInformation()
         }
         if($result.Contains("Valid from:"))
         {
-            $certificateValidDate = $result.Substring(48)
+            $certificateValidDate = ParseExpirationDate $result
             $trustStoreCertificate =[pscustomobject]@{
                 alias = $certificateAlias
                 validDate = $certificateValidDate
